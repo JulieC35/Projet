8ccb4f9a3a3b64a0bb8166c2a2eb6d78bcd173e4
JulieC35 <chapdelainejulie0@gmail.com>
2017-06-13 16:00:17 +0200

    Modifications

--- src/QueryResult.java
+++ src/QueryResult.java
@@ -1,3 +1,21 @@
+import java.util.*;
+
 public class QueryResult{
-	
-}
+	private ArrayList<Column> schema;
+	private ArrayList<Row> rows;
+
+	public QueryResult(){
+
+	}
+
+	public Row[] getRows(){
+		Row[] ret;
+		ret = (Row[])this.rows.toArray();
+		return ret;
+	}
+
+	public String toString(){
+		String s = "";
+		return s;
+	}
+}	
