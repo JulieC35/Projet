688c8a90c1090f6854016b30012efec6bde88613
JulieC35 <chapdelainejulie0@gmail.com>
2017-06-13 16:16:49 +0200

    Modifications

--- src/Table.java
+++ src/Table.java
@@ -5,6 +5,11 @@ public class Table{
 	private ArrayList<Column> schema;
 	private ArrayList<Row> rows;
 
+	/**
+	 * The constructor
+	 * @param name the name of the table
+	 * @param schema all the column of the table
+	 */
 	public Table(String name, Column[] schema){
 		if(name != null){
 			this.name = name;
@@ -17,6 +22,73 @@ public class Table{
 			this.rows = new ArrayList<Row>();
 		}
 	}
+
+	/**
+	 * Getter of the name
+	 * @return the name of the current table
+	 */
+	public String getName(){
+		return this.name;
+	}
+
+	/**
+	 * Setter of the name
+	 * @param the new name of the table
+	 */
+	public void setName(String name){
+		if(name != null){
+			this.name = name;
+		}
+	}
+
+	/**
+	 * Getter of the schema 
+	 * @return a table of all the column
+	 */
+	public String[] getSchema(){
+		return null;
+	}
+
+	/**
+	 * Getter of the rows
+	 * @return all the rows in the table
+	 */
+	public Row[] getRows(){
+		return null;
+	}
+
+	/**
+	 * Add a row in the table
+	 * @param row the row to add
+	 */
+	public void addRow(Row r){
+
+	}
+
+	/**
+	 * Delete a row from the table
+	 * @param id the id of the row to delete
+	 */
+	public void deleteRow(int id){
+
+	}
+
+	/**
+	 * Clear the table
+	 */
+	public void empty(){
+
+	}
+
+	/**
+	 * Print a description of the object
+	 * @return the description
+	 */
+	public String toString(){
+		String s = "";
+		return s;
+	}
+
 
 
 }
