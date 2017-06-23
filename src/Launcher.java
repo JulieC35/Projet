ff6e2c0561bc6c29bd7993d83594f534f87d9a41
Marem <marclamy.bzh@gmail.com>
2017-06-23 20:22:01 +0200

    FRENCH.xml

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,24 +2,20 @@
  * Launcher of the application. If console argument is recieved, the GUI is not built
  */
 import application.console.*;
+import application.graphical.*;
 import lang.*;
 
-public class Launcher{
+public class Launcher extends Application{
     /**
      * Entry point
      * @param args The lsit of arguments of the application
      */
     public static void main(String[]args){
-        if ( args.length > 0 && args[0].equals("console") )
-            new ConsoleApplication();
+        if ( args.length > 0 && args[0].equals("gui") ) {
+            GraphicalApplication app = new GraphicalApplication();
+            app.launch(args);
+        }
         else
-            runGUI();
-    }
-
-    /**
-     * GUI placeholder
-     */
-    public static void runGUI(){
-        System.out.println(L.get("launcher-gui-mode"));
+            new ConsoleApplication();
     }
 }
