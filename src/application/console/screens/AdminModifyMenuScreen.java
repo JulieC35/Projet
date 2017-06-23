e6db61e709955c32fddd561af704b3f4f13a2b2d
JulieC35 <chapdelainejulie0@gmail.com>
2017-06-23 19:36:19 +0200

    Modification de l'appli console pour le panneau Admin

---
+++ src/application/console/screens/AdminModifyMenuScreen.java
@@ -0,0 +1,52 @@
+/**
+ * Users list screen
+ */
+package application.console.screens;
+
+import application.console.*;
+import model.*;
+import model.entities.*;
+import lang.*;
+import java.util.*;
+
+public class AdminModifyMenuScreen extends TerminalScreen{
+
+	public AdminModifyMenuScreen(ConsoleApplication terminal, Application app){
+		super(terminal, app);
+	}
+
+	public void initialize(){
+        terminal.printHeader();
+        terminal.printTitle(app.getConnectionProfile().getName() + " : " + L.get("admin-list"));
+        terminal.printMessage();
+        terminal.printMenu(new String[]{L.get("user-to-admin"), L.get("user-to-restricted")});
+        terminal.startPrompting();
+
+        this.exit();
+    }
+
+    /**
+     * Executes the recieved request
+     * @param request An array of strings with the command and its parameters
+     * @return The result of the request as a RequestResult element
+     */
+    public RequestResult proceedRequest(String[] request){
+        RequestResult ret = super.proceedRequest(request);
+
+        if ( ret == RequestResult.OK ){
+            try {
+                if (Integer.parseInt(request[0]) == 1){
+                    terminal.setCurrentScreen(new AdminUpScreen(terminal, app));
+                }
+                else if(Integer.parseInt(request[0]) == 1){
+                	terminal.setCurrentScreen(new AdminLowScreen(terminal, app));
+                }
+            } catch (NumberFormatException ex ){
+                terminal.setMessage(L.get("not-valid-input"));
+                ret = RequestResult.ERROR;
+            }
+        }
+        return ret;
+    }
+
+}
