c4a8aaf38eddbb760b5bbde35358a9f92d43fd33
Marem <marclamy.bzh@gmail.com>
2017-06-23 20:34:40 +0200

    Admin panel ENGLISH.XML

--- src/Launcher.java
+++ src/Launcher.java
@@ -5,15 +5,14 @@ import application.console.*;
 import application.graphical.*;
 import lang.*;
 
-public class Launcher extends Application{
+public class Launcher{
     /**
      * Entry point
      * @param args The lsit of arguments of the application
      */
     public static void main(String[]args){
         if ( args.length > 0 && args[0].equals("gui") ) {
-            GraphicalApplication app = new GraphicalApplication();
-            app.launch(args);
+            GraphicalApplication.main(args);
         }
         else
             new ConsoleApplication();
